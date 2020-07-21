6b53e382110f41e5f6acfc550ac57ccc0e7ad33f
dhakalsamundra <seapush02@gmail.com>
2020-07-21 20:10:20 +0300

    add signup reducer

--- client/src/redux/reducers/user.ts
+++ client/src/redux/reducers/user.ts
@@ -1,7 +1,8 @@
-import { GOOGLE_SIGNIN, UserActions, SIGNOUT, UserState } from '../../types'
+import { GOOGLE_SIGNIN, UserActions, SIGNOUT, UserState, REGISTER_USER } from '../../types'
 
 export default function user(
-  state: UserState = { users: [] },
+  state: UserState = { users: []
+  },
   action: UserActions
 ): UserState {
   switch (action.type) {
@@ -11,6 +12,10 @@ export default function user(
     }
     case SIGNOUT: {
       return state
+    }
+    case REGISTER_USER: {
+      const {user} = action.payload
+      return { ...state, users: [...state.users, user]}
     }
 
     default:
