559dee3082e1c1718c23d556553fda742f4ec11e
dhakalsamundra <seapush02@gmail.com>
2020-07-21 20:07:11 +0300

    add register page

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -4,11 +4,14 @@ import { Switch, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import LandingPage from './pages/landing'
 import Author from './components/Author'
+import Register from './components/SignUpForm'
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={LandingPage} />
     <Route exact path="/home" component={Home} />
     <Route exact path="/author" component={Author} />
+    <Route exact path="/register" component={Register} />
+
   </Switch>
 )
 
