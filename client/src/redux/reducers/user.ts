cec0a0022e3b017c6aa6c08ef3cfd6a9ab39dee6
dhakalsamundra <seapush02@gmail.com>
2020-07-23 13:04:24 +0300

    add boolean in state

--- client/src/redux/reducers/user.ts
+++ client/src/redux/reducers/user.ts
@@ -1,7 +1,7 @@
 import { GOOGLE_SIGNIN, UserActions, SIGNOUT, UserState, REGISTER_USER, SIGNIN } from '../../types'
 
 export default function user(
-  state: UserState = { users: []
+  state: UserState = { users: [], isAuthorized: false
   },
   action: UserActions
 ): UserState {
