320686e00129f5ccf9f545162f67544583df5bbe
dhakalsamundra <seapush02@gmail.com>
2020-07-28 15:48:10 +0300

    add normal user home page

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -5,12 +5,14 @@ import Home from './pages/Home'
 import LandingPage from './pages/landing'
 import Author from './components/Author'
 import Register from './components/SignUpForm'
+import userHome from './pages/userHome'
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={LandingPage} />
     <Route exact path="/home" component={Home} />
     <Route exact path="/author" component={Author} />
     <Route exact path="/register" component={Register} />
+    <Route exact path="/dashboard" component={userHome} />
 
   </Switch>
 )
