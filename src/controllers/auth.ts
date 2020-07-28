d50c404d77728081a2bf212c399f6350b9022be4
dhakalsamundra <seapush02@gmail.com>
2020-07-28 16:05:09 +0300

    add send forget resetLink to user email

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,16 +1,27 @@
 import { Request, Response, NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
+import sgMail from '@sendgrid/mail'
 
-import { JWT_SECRET } from '../util/secrets'
+import { JWT_SECRET, SENDGRID_API_KEY, FROM_MAIL } from '../util/secrets'
 import User, { UserDocument } from '../models/User'
 import AuthService from '../services/auth'
-import { BadRequestError, InternalServerError } from '../helpers/apiError'
+import {
+  BadRequestError,
+  InternalServerError,
+  NotFoundError,
+} from '../helpers/apiError'
+import validator from 'validator'
+import sendEmail from '../util/sendResetEmail'
+import { jwtToken } from '../util'
 
-
+type passwordRequest = {
+  email: string;
+  url: string;
+}
 const isEmail = (email: string) => {
   const regEx = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
-  if(email.match(regEx)) {
+  if (email.match(regEx)) {
     return true
   } else {
     return false
@@ -19,7 +30,7 @@ const isEmail = (email: string) => {
 
 const isPassword = (password: string) => {
   const regEx = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
-  if(password.match(regEx)) return true
+  if (password.match(regEx)) return true
   else return false
 }
 export const authenticateUser = async (req: Request, res: Response) => {
@@ -29,7 +40,7 @@ export const authenticateUser = async (req: Request, res: Response) => {
     lastName,
     picture,
     userName,
-    role
+    role,
   } = req.user as UserDocument
   const token = jwt.sign(
     { email, firstName, userName, lastName, picture, role },
@@ -49,21 +60,23 @@ export const signUp = async (
   try {
     const { firstName, lastName, email, userName, password } = req.body
     let role
-    if(email === 'dhakalsamundra35@gmail.com') {
+    if (email === 'dhakalsamundra35@gmail.com') {
       role = 'superadmin'
     }
-    if(email.includes('@integrify.io')) {
+    if (email.includes('@integrify.io')) {
       role = 'admin'
     }
     //Password hashing
     const salt = await bcrypt.genSalt(10)
     const hash = await bcrypt.hash(password, salt)
 
-    if(!isEmail(email)) {
+    if (!isEmail(email)) {
       throw new Error('Must be a valid email address')
     }
-    if(!isPassword(password)) {
-      throw new Error('Password must have length of 8 character with one uppercase, one lowercase, one digit and one special character ')
+    if (!isPassword(password)) {
+      throw new Error(
+        'Password must have length of 8 character with one uppercase, one lowercase, one digit and one special character '
+      )
     }
     const user = new User({
       firstName,
@@ -71,7 +84,7 @@ export const signUp = async (
       email,
       userName,
       role,
-      password: hash
+      password: hash,
     })
 
     const savedUser = await AuthService.create(user)
@@ -91,25 +104,79 @@ export const signIn = async (
   next: NextFunction
 ) => {
   try {
-  const {email, password} = req.body
-  const user = await User.findOne({email})
-  if(!user){
-    throw new BadRequestError('email is not register in the DataBase')
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
+    if (!user) {
+      throw new BadRequestError('email is not register in the DataBase')
+    }
+    const result = await bcrypt.compare(password, user.password)
+    if (!result) {
+      throw new BadRequestError('email is not registered')
+    } else {
+      const token = jwt.sign(
+        {
+          email: user.email,
+          userName: user.userName,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          role: user.role,
+        },
+        JWT_SECRET,
+        { expiresIn: '1h' }
+      )
+      res.json(token)
+    }
+  } catch (error) {
+    next(error)
   }
-  const result = await bcrypt.compare(password, user.password)
-  if(!result ) {
-    throw new BadRequestError('email is not registered')
-  } else {
-    const token = jwt.sign({
-      email: user.email,
-      userName: user.userName,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      role: user.role
-    }, JWT_SECRET, { expiresIn: '1h'})
-    res.json(token)
-  } 
-} catch (error) {
-  next(error)
 }
+
+export const passwordRequestReset = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email } = req.body
+  const user = await User.findOne({ email })
+
+  if (user) {
+    await user.generatePasswordReset()
+    try {
+      await user.save()
+
+      const link = `http://${req.headers.host}/api/v1/auth/reset-password/${user.resetPasswordToken}`
+      sgMail.setApiKey(SENDGRID_API_KEY)
+
+      const mailOptions = {
+        to: user.email,
+        from: FROM_MAIL,
+        subject: 'Password change request',
+        text: `Hi ${user.userName},
+          Please click on the following link ${link} to reset your password.
+          
+          If you did not request this, please ignore this email and your password will remain unchanged.`,
+      }
+
+      try {
+        const sendMail = await sgMail.send(mailOptions)
+        if (sendMail) {
+          res.json({
+            message: `A reset email has been sent to ${user.email} .`,
+          })
+        } else {
+          next(new BadRequestError('Reset Email could not be sent'))
+        }
+      } catch (error) {
+        next(new BadRequestError(error))
+      }
+    } catch (error) {
+      next(new BadRequestError(error))
+    }
+  } else {
+    next(
+      new NotFoundError(
+        `The email address, ${email} is not associated with any account.`
+      )
+    )
+  }
 }
